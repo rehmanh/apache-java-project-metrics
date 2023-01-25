2ed904628e889a943db4d5fa3dbee91d15818638
Habib ur Rehman <hbieb949@gmail.com>
2023-01-25 10:52:56 -0500

    Added Num source files and num LOC to metric collection

--- ApacheMetricCollection.py
+++ ApacheMetricCollection.py
@@ -21,18 +21,19 @@ class ApacheMetricCollection:
         return repos
 
     def clone_and_process_gh_repos(self, repos: list) -> None:
-        # repo[0] = name
-        # repo[1] = url
         for repo in repos:
+            name = repo[0]
+            url = repo[1]
+
             process = subprocess.Popen(
-                ["git", "clone", repo[1], repo[0]], 
+                ["git", "clone", url, name], 
                 cwd="./repositories", 
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True
             )
 
-            print(f"Cloning the project {repo[0]}...")
+            print(f"Cloning the project {name}...")
             process.wait()
 
             # check the output and error of the clone command
@@ -40,17 +41,19 @@ class ApacheMetricCollection:
 
             if (process.poll() == 0):
                 # clone successful -- want to begin processing the gitlog files
-                num_revisions = self.get_project_revision_count(repo[0])
-                num_authors = self.get_project_author_count(repo[0])
-                first_revision_date = self.get_first_commit_date(repo[0])
-                self.write_data_to_csv(repo[0], num_revisions, num_authors, first_revision_date)
+                num_revisions = self.get_project_revision_count(name)
+                num_authors = self.get_project_author_count(name)
+                num_source_files = self.get_num_source_files(name)
+                num_source_loc = self.get_source_loc(name)
+                first_revision_date = self.get_first_commit_date(name)
+                self.write_data_to_csv(name, num_revisions, num_authors, num_source_files, num_source_loc, first_revision_date)
             elif(process.poll() == 128):
                 # clone unsuccessful -- print error and continue
-                print(f"Repository for project {repo[0]} was not found on GitHub")
-                self.write_data_to_csv(repo[0], 0, 0, 0)
+                print(f"Repository for project {name} was not found on GitHub")
+                self.write_data_to_csv(name, 0, 0, 0, 0, 0)
 
-            print(f"Collected metrics for {repo[0]}; removing the directory ./repositories/{repo[0]}...\n")
-            self.remove_repo_directory(repo[0])
+            print(f"Collected metrics for {name}; removing the directory ./repositories/{name}...\n")
+            self.remove_repo_directory(name)
                 
 
     def get_project_revision_count(self, dir_name: str) -> int:
@@ -89,6 +92,56 @@ class ApacheMetricCollection:
             return int(output)
         else:
             print(error)
+        
+    def get_num_source_files(self, dir_name: str) -> int:
+        p1 = subprocess.Popen(
+            ["find", ".", "-iname", "*.java", "-not", "-path", "*/.*"],
+            cwd=f"./repositories/{dir_name}",
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        p2 = subprocess.Popen(
+            ["wc", "-l"],
+            cwd=f"./repositories/{dir_name}",
+            stdin=p1.stdout,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+        output, error = p2.communicate()
+        if output is not None:
+            return int(output)
+        else:
+            print(error)
+    
+    def get_source_loc(self, dir_name: str) -> int:
+        #find . -iname '*.java' | xargs cat | wc -l
+        p1 = subprocess.Popen(
+            ["find", ".", "-iname", "*.java"],
+            cwd=f"./repositories/{dir_name}",
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        p2 = subprocess.Popen(
+            ["xargs", "cat"],
+            cwd=f"./repositories/{dir_name}",
+            stdin=p1.stdout,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        p3 = subprocess.Popen(
+            ["wc", "-l"],
+            cwd=f"./repositories/{dir_name}",
+            stdin=p2.stdout,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+        output, error = p3.communicate()
+        if output is not None:
+            return int(output)
+        else:
+            print(error)
     
     def get_first_commit_date(self, dir_name: str) -> str:
         # git log --pretty=format:'%ad' | tail -1
@@ -118,15 +171,15 @@ class ApacheMetricCollection:
     def remove_repo_directory(self, dir_name: str) -> None:
         shutil.rmtree(f"./repositories/{dir_name}", ignore_errors=True)
     
-    def write_data_to_csv(self, repo_name: str, num_revisions: int, num_authors: str, first_revision_date: str) -> None:
+    def write_data_to_csv(self, repo_name: str, num_revisions: int, num_authors: str, num_source_files: int, num_source_loc: int, first_revision_date: str) -> None:
         with open('csv/ApacheProjectMetrics.csv', 'a') as file:
             writer = csv.writer(file, csv.QUOTE_NONNUMERIC)
-            writer.writerow([repo_name, num_revisions, num_authors, first_revision_date])
+            writer.writerow([repo_name, num_revisions, num_authors, num_source_files, num_source_loc, first_revision_date])
     
     def create_csv_file(self) -> None:
         with open('csv/ApacheProjectMetrics.csv', 'w') as file:
             writer = csv.writer(file, csv.QUOTE_NONNUMERIC)
-            writer.writerow(['Project Name', 'Number of Revisions', 'Number of Authors', 'Date of First Commit'])
+            writer.writerow(['Project Name', 'Number of Revisions', 'Number of Authors', 'Number of Source Files', 'Number of Source LOC', 'Date of First Commit'])
         
 
 if __name__ == '__main__':
